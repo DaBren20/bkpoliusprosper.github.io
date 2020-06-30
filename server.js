8354c56ccb39176eda404790c296f70697c622e1
DaBren20 <dabrentwenty@gmail.com>
2020-06-29 20:07:01 -0400

    first commit

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const expbs = require('express-handlebars');
 const path = require("path");
 //const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
-const { check, validationResult } = require('express-validator');
+//const { check, validationResult } = require('express-validator');
 
 //load the environment variable file
-require('dotenv').config({path:"./config/keys.env"});
+require('dotenv').config({
+    path:"./config/keys.env"
+});
 
 //let urlencoded = bodyParser.urlencoded({ urlencoded: false});
 app.engine('handlebars', expbs( { defaultLayout: 'main' }));
@@ -71,7 +73,7 @@ app.post('/login', (req,res) => {
     if(error2.length > 0) {
         res.render('login', { 
             title: 'Login',
-            errorMessages: error2
+            errorMessages2: error2
         });
         return;
     }
@@ -82,79 +84,6 @@ app.post('/login', (req,res) => {
     }
 });
 
-/*app.post('/signup', (req,res) => {
-
-    const error1 = [];
-    const error2 = [];
-    const error3 = [];
-    const error4 = [];
-
-    if(req.body.firstname == "") {
-        error1.push("This field is required.");
-    }
-
-    if(req.body.lastname == "") {
-        error2.push("This field is required");
-    }
-
-    if(req.body.email == "") {
-        error1.push("This field is required.");
-    }
-
-    if(req.body.password == "") {
-        error2.push("This field is required");
-    }
-
-    //This is if the user failed validation
-    if(error1.length > 0) {
-        res.render('signup', { 
-            title: 'Create your account',
-            errorMessages: error1 
-        });
-        return;
-    }
-
-    else {
-        res.redirect("/splash");
-    }
-
-    if(error2.length > 0) {
-        res.render('signup', { 
-            title: 'Create your account',
-            errorMessages: error2
-        });
-        return;
-    }
-
-    else {
-        res.redirect("/splash");
-    }
-
-    if(error3.length > 0) {
-        res.render('signup', { 
-            title: 'Create your account',
-            errorMessages: error3
-        });
-        return;
-    }
-
-    else {
-        res.redirect("/splash");
-    }
-
-    if(error4.length > 0) {
-        res.render('signup', { 
-            title: 'Create your account',
-            errorMessages: error4
-        });
-        return;
-    }
-
-    else {
-        res.redirect("/splash");
-    }
-
-});*/
 
 app.listen(8080, () => {
     console.log("Express http server listening on: ", PORT);
